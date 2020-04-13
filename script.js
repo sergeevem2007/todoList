9b71fae7022de34447f07effebb810b973034f46
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2020-04-13 22:52:02 +0400

    feat:do first part of lesson

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 'use strict';
+let todoControl = document.querySelector('.todo-control'),
+    headerInput = document.querySelector('.header-input'),
+    todoList = document.querySelector('.todo-list'),
+    todoCompleted = document.querySelector('.todo-completed');
+
+let todoData = [];
+
+let render = function() {
+  todoList.textContent = '';
+  todoCompleted.textContent = '';
+
+  todoData.forEach(function(item){
+    let li = document.createElement('li');
+    li.classList.add('todo-item');
+
+    li.innerHTML = '<span class="text-todo">' + item.value +'</span>' +
+      '<div class="todo-buttons">' +
+        '<button class="todo-remove"></button>' +
+        '<button class="todo-complete"></button>' +
+      '</div>';
+
+      if (item.completed) {
+        todoCompleted.append(li);
+      } else {
+        todoList.append(li);
+      }
+    
+    let btnTodoCompleted = li.querySelector('.todo-complete');
+    btnTodoCompleted.addEventListener('click', function(){
+      item.completed = !item.completed;
+      render();
+    });
+  });
+};
+
+todoControl.addEventListener('submit' , function(event) {
+  event.preventDefault();
+
+  let newTodo = {
+    value: headerInput.value,
+    completed: false
+  };
+  todoData.push(newTodo);
+
+  render();
+});
+
+render();
